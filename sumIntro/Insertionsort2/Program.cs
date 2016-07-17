01bb216f7d14b059804963a283001e86298724e5
leo <leonelfmir@gmail.com>
2016-07-17 12:35:25 -0400

    to

---
+++ sumIntro/Insertionsort2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insertionsort2
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            int _ar_size;
+            _ar_size = Convert.ToInt32(Console.ReadLine());
+            int[] _ar = new int[_ar_size];
+            String elements = Console.ReadLine();
+            String[] split_elements = elements.Split(' ');
+            for (int _ar_i = 0; _ar_i < _ar_size; _ar_i++)
+            {
+                _ar[_ar_i] = Convert.ToInt32(split_elements[_ar_i]);
+            }
+
+            insertionSort(_ar);
+        }
+
+        static
+    }
+}
