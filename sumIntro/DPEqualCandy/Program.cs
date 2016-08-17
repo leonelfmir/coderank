48cbb09263f98ac3148916bbc60c011eca74298b
leonel <leoenlfmir@hotmail.com>
2016-08-17 15:28:25 -0400

    all

--- sumIntro/DPEqualCandy/Program.cs
+++ sumIntro/DPEqualCandy/Program.cs
@@ -17,17 +17,45 @@ namespace DPEqualCandy
                 int[] A = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
                 Console.WriteLine(sol(n, A));
             }
+
+            Console.Read();
         }
 
         static int sol(int n, int[] A)
         {
             Array.Sort(A);
 
-            int diff = 0;
-            for(int i = 0; i < n; i++)
+            int res = 0;
+            for(int i = 1; i < n; i++)
             {
-
+                if(A[i-1] < A[i])
+                {
+                    int df = A[i] - A[i - 1];
+                    res += getDiff(df);
+                    A[i] -= df;
+                }
             }
+
+            return res;
+        }
+
+        static int getDiff(int diff)
+        {
+            int res = 0;
+            int ch = 3;
+            int[] chs = { 1, 2, 5 };
+
+            int mod = diff;
+            int div = 0;
+            for (int i = ch - 1; i >= 0; i--)
+            {
+                div = mod / chs[i];
+                mod = mod % chs[i];
+                res += div;
+            }
+
+            return res;
+            
         }
     }
 }
