550e26ac8f99ef8f8f60af7db3e419240c52284a
leonel <leoenlfmir@hotmail.com>
2016-08-18 10:21:12 -0400

    all

---
+++ sumIntro/DPEqualCandy2/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPEqualCandy2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] B = { 1, 5, 5 };
+            Console.WriteLine(sol(B.Length, B));
+            int T = int.Parse(Console.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                int[] A = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                Console.WriteLine(sol(n, A));
+            }
+
+            Console.Read();
+        }
+
+        static int sol(int n, int[] A)
+        {
+            int res = 0;
+            bool Change = true;
+            while (Change)
+            {
+                Array.Sort(A);
+                int i = 1;
+                for (; i < n; i++)
+                {
+                    if (A[i - 1] < A[i])
+                    {
+                        int df = A[i] - A[i - 1];
+                        res += getDiff(df, A, i, out Change);
+
+                        if (Change)
+                            break;
+                    }
+
+                }
+                if (i == n)
+                    Change = false;
+            }
+
+            return res;
+        }
+
+        static int getDiff(int diff, int[] A, int pos, out bool change)
+        {
+            int res = 0;
+            int ch = 3;
+            int[] chs = { 1, 2, 5 };
+            int sum = 0;
+            int mod = diff;
+            int div = 0;
+
+            if (diff > 4)
+            {
+                div = mod / chs[2];
+                mod = mod % chs[2];
+                res += div;
+                sum = div * chs[2];
+            }
+
+            if(mod == 4)
+            {
+                res++;
+                sum += 5;
+                change = true;
+            }
+            else
+            {
+                change = false;
+                for (int i = ch - 2; i >= 0; i--)
+                {
+                    div = mod / chs[i];
+                    mod = mod % chs[i];
+                    res += div;
+                    sum += div * chs[i];
+                }
+            }
+
+            addToArr(A, pos, sum);
+
+            return res;
+
+        }
+
+        static void addToArr(int[] A, int pos, int amnt)
+        {
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (i == pos)
+                    continue;
+
+                A[i] += amnt;
+            }
+        }
+    }
+}
+
--- sumIntro/DPEqualCandy3/Program.cs
+++ sumIntro/DPEqualCandy3/Program.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DPEqualCandy
+namespace DPEqualCandy3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] B = { 1,5,5 };
+            //int[] B = { 1, 5, 5 };
+            string bb = "1 5 5";
+            int[] B = Array.ConvertAll(bb.Split(' '), int.Parse);
             Console.WriteLine(sol(B.Length, B));
+
+
             int T = int.Parse(Console.ReadLine());
             for (int i = 0; i < T; i++)
             {
@@ -27,18 +31,35 @@ namespace DPEqualCandy
         {
             Array.Sort(A);
 
-            int res = 0;
-            for(int i = 1; i < n; i++)
+            //int res0 = 0;
+            //int res1 = 0;
+            //int res2 = 2;
+            //int min0 = A[0];
+            //int min1 = A[0] - 1;
+            //int min2 = A[0] - 2;
+
+            int[] chs = { 0, 1, 2 };
+            int[] res = new int[chs.Length];
+            int min = A[0];
+
+            for (int i = 0; i < n; i++)
             {
-                if(A[i-1] < A[i])
+                for (int j = 0; j < chs.Length; j++)
                 {
-                    int df = A[i] - A[i - 1];
-                    res += getDiff(df);
-                    A[i] -= df;
+                    if(A[i] > min - j)
+                        res[j] += getDiff(A[i] - min + j);
                 }
+                //if (A[i] > min0)
+                //{
+                //    res0 += getDiff(A[i]-min0);
+                //}
+                //if (A[i] > min1)
+                //{
+                //    res1 += getDiff(A[i] - min1);
+                //}
             }
 
-            return res;
+            return res.Min();
         }
 
         static int getDiff(int diff)
@@ -47,7 +68,7 @@ namespace DPEqualCandy
             int ch = 3;
             int[] chs = { 1, 2, 5 };
 
-            int mod = diff;
+            int mod = Math.Abs(diff);
             int div = 0;
             for (int i = ch - 1; i >= 0; i--)
             {
@@ -57,7 +78,7 @@ namespace DPEqualCandy
             }
 
             return res;
-            
+
         }
     }
 }
